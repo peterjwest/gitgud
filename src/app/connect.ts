a49a487b14df83dcae56b3bce5d15783de1c2bfc
Peter West <peterjwest3@gmail.com>
2018-08-28 14:11:05 +0100

    Fix connect function so it respects own props

--- src/app/connect.ts
+++ src/app/connect.ts
@@ -2,16 +2,21 @@ import * as React from 'react';
 import { connect as defaultConnect } from 'react-redux';
 import { Dispatch, Action } from 'redux';
 
-export function connect<
-  Store, StoreProps, Actions extends Action,
-  ComponentClass extends React.ComponentClass<Props, State>, Props extends StoreProps, State
->
-(component: ComponentClass, mapStoreProps: (store: Store) => StoreProps) {
-  return defaultConnect<StoreProps, ActionProps<Actions>>(mapStoreProps,
-    (dispatch: Dispatch<Actions>): ActionProps<Actions> => ({ dispatch: dispatch }),
-  )(component);
-}
-
 export interface ActionProps<Actions extends Action> {
   dispatch: Dispatch<Actions>;
+}
+
+export function connect<
+  Store,
+  ComponentClass extends React.ComponentClass<StoreProps & ActionProps<Actions> & ComponentProps, State>,
+  StoreProps,
+  ComponentProps,
+  Actions extends Action,
+  State,
+>
+(component: ComponentClass, mapStoreProps: (store: Store) => StoreProps): React.ComponentClass<ComponentProps> {
+  return defaultConnect<StoreProps, ActionProps<Actions>, ComponentProps>(
+    mapStoreProps,
+    (dispatch: Dispatch<Actions>): ActionProps<Actions> => ({ dispatch: dispatch }),
+  )(component);
 }
